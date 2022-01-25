0b14d76d5a1ecc9fa14d5d3ad66db45f5b7a851c
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-01-25 15:17:51 +0100

    adding upsert funcionality with view

---
+++ Asp-Net/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿
+using Asp_Net.DataAcess;
+using Asp_Net.DataAcess.Repository.IRepository;
+using Asp_Net.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Asp_Net.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+
+            return View(objProductList);
+        } 
+
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+                );
+
+            if (id == null || id == 0)
+            {
+                ViewBag.CategoryList = CategoryList;
+                return View(product);
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product item)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(item);
+                _unitOfWork.Save();
+                TempData["success"] = "Product edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var ProductFromDb = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var item = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(item);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
