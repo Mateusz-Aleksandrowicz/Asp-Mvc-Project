434c05e8c80098b41dde514d144df3623a03c09c
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-01-27 14:00:55 +0100

    adding Identity to project and Db

--- Asp-Net.DataAcess/Data/ApplicationDbContext.cs
+++ Asp-Net.DataAcess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Asp_Net.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Asp_Net.DataAcess
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- Asp-Net/Program.cs
+++ Asp-Net/Program.cs
@@ -2,6 +2,7 @@ using Asp_Net.DataAcess;
 using Asp_Net.DataAcess.Repository;
 using Asp_Net.DataAcess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddMvc().AddNToastNotifyToastr();
@@ -28,7 +31,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
