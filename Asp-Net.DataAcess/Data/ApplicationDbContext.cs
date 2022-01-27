1548d3060c90aa8f68aabb0cc2108b929f51ad9b
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-01-27 15:40:44 +0100

    adding roles Admin,Customer

--- Asp-Net.DataAcess/Data/ApplicationDbContext.cs
+++ Asp-Net.DataAcess/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace Asp_Net.DataAcess
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
--- Asp-Net.DataAcess/Migrations/20220127132223_newIdentityUserColumns.Designer.cs
+++ Asp-Net.DataAcess/Migrations/20220127132223_newIdentityUserColumns.Designer.cs
@@ -4,6 +4,7 @@ using Asp_Net.DataAcess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Asp_Net.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220127132223_newIdentityUserColumns")]
+    partial class newIdentityUserColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
